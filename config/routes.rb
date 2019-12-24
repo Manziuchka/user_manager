1732f751ee6fccc21cfb40af76bf9e90ded1dcc4
Manziuchka <Manziuk.olga@gmail.com>
2019-12-24 18:30:03 +0700

    Admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index', as: 'home'
-  resources :users
+  resources :users, only: [:show, :index]
+
+  namespace :admin do
+    resources :users, except: [:index]
+  end
 end
