5cb6994b9e439bad42b1c7d1082c4d3e52420d31
Manziuchka <Manziuk.olga@gmail.com>
2019-12-25 22:54:43 +0700

    Add validation

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+
     def new
         @user = User.new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :registerable,
          :rememberable,
          :validatable
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
 end
