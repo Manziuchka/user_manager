c7c10160fe8bf803f783e55d554444935560363f
Manziuchka <Manziuk.olga@gmail.com>
2020-01-07 01:06:27 +0700

    add seeds-example

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'admin', email: 'admin@gmail.com', password: 'adminadmin', admin: true)
+
+1.upto(10) do |f|
+    name = 'User ' + f.to_s
+    email = 'user'+ f.to_s + '@gmail.com'
+    User.create(name: name, email: email, password: '123456')
+end
